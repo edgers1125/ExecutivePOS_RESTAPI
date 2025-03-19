6679799cd7c05e374150407cb55a4c3c41b43bf2
edgers1125 <ejnavarro555@gmail.com>
2025-03-19 12:27:19 +0800

    Initial Commit

---
+++ src/main/java/com/executivePOS/RestAPI/RestApiApplication.java
@@ -0,0 +1,13 @@
+package com.executivePOS.RestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/executivePOS/RestAPI/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.executivePOS.RestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
